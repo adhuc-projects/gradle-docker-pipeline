2d96a18b608265be430610f97246d845c99765de
Alexandre Carbenay <acarbenay@adhuc.fr>
2018-12-12 13:20:43 +0100

    Configure sample controller based on spring boot initializr project

---
+++ src/main/java/org/adhuc/sample/gradledockerpipeline/GradleDockerPipelineApplication.java
@@ -0,0 +1,12 @@
+package org.adhuc.sample.gradledockerpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDockerPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDockerPipelineApplication.class, args);
+	}
+}
---
+++ src/test/java/org/adhuc/sample/gradledockerpipeline/GradleDockerPipelineApplicationTest.java
@@ -0,0 +1,16 @@
+package org.adhuc.sample.gradledockerpipeline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradleDockerPipelineApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
