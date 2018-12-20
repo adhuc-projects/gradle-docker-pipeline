5e3972cc5b5d84bf616573d93033cefd1578859d
Alexandre Carbenay <acarbenay@adhuc.fr>
2018-12-20 20:16:29 +0100

    Run docker-compose with random port during test execution

--- src/acceptance/java/org/adhuc/sample/gradledockerpipeline/HelloWorldAcceptanceTest.java
+++ src/acceptance/java/org/adhuc/sample/gradledockerpipeline/HelloWorldAcceptanceTest.java
@@ -1,6 +1,8 @@
 package org.adhuc.sample.gradledockerpipeline;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -9,10 +11,18 @@ import static org.junit.Assert.assertEquals;
 
 public class HelloWorldAcceptanceTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldAcceptanceTest.class);
+
+    static final String SERVICE_NAME = "gradle-docker-pipeline";
+    static final String EXPOSED_PORT = "8080";
+    static final String PORT_PROPERTY_NAME = SERVICE_NAME + ".tcp." + EXPOSED_PORT;
+
     @Test
     public void callHelloWorld() {
+        String port = System.getProperty(PORT_PROPERTY_NAME);
+        LOGGER.info("Call hello world with port {}", port);
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080", String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port, String.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("Hello World!", response.getBody());
     }
